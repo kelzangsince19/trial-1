083d22312b86a0efb5de469de13b455c2e0f77cb
kelzangsince19 <02230097.cst@rub.edu.bt>
2024-02-26 14:51:09 +0600

    logical operator

---
+++ logical.py
@@ -0,0 +1,25 @@
+# example 1
+x = 5
+print(x > 3 and x < 10)
+
+#example 2
+y = 12
+print(y > 10 and y % 5 == 0)
+
+# or operator
+# example 1
+x = 5
+print(x < 3 or x > 10)
+
+#example 2
+y = 12
+print(y < 10 or y % 2 == 0)
+
+#not operator
+# example 1
+x = 5
+print(not(x > 3 and x < 10))
+
+#example 2
+y = 12
+print(not(y > 10 and y % 5 == 0))
