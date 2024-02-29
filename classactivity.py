cf2179b2145966a791c2fd7ec1f9001340e01f72
kelzangsince19 <02230097.cst@rub.edu.bt>
2024-02-29 11:18:14 +0600

    activity commit

---
+++ classactivity.py
@@ -0,0 +1,8 @@
+print("Hola")
+k=input("enter your  name")
+num1=float(input("enter a number"))
+num2=float(input("enter a number"))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 % num2)
