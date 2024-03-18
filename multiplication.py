9d070709b866dd85194609781a65eee81f1a6b83
kelzangsince19 <02230097.cst@rub.edu.bt>
2024-03-18 15:58:23 +0600

    work

--- multiplication.py
+++ multiplication.py
@@ -1,6 +1,5 @@
-#multiplication table from 1 to 12
-num = 5
-num = int(input("show the multiplication table of? "))
-#iterate 12 times from i = 1 to i = 13
-for i in range(1, 13):
-    print(num, 'x', i, '=', num*i)
+number = int(input("Enter the number for which you want the multiplication table: "))
+limit = int(input("Enter the limit up which you want the multiplication table generated: "))
+
+for i in range(1, limit + 1):
+    print(f"{number} * {i} = {number * i}")
